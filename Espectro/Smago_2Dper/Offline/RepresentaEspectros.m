9e041d2aa5669b97f0e7bf8974ad0cdfab9b2547
Alejandro Bandera <abandera@us.es>
2024-05-22 17:40:56 +0200

    Cálculo del espectro FOM

--- Espectro/Smago_2Dper/Offline/RepresentaEspectros.m
+++ Espectro/Smago_2Dper/Offline/RepresentaEspectros.m
@@ -2,9 +2,13 @@
 
 NN = [34,67,88,96,108,109];
 Re = 1000:625:16000;
-
+addpath 'DataCompletePOD'
 figure()
 hold on
+fID = fopen('EspectroFOM.txt','r');
+vec = fscanf(fID, '%f');
+sp = vec(2:end);
+plot(Re,sp,'--','Linewidth',2)
 grid on
 for i=1:length(NN)
     fID = fopen(['DeltaN_',num2str(NN(i)),'.txt'],'r');
@@ -13,16 +17,19 @@ for i=1:length(NN)
     plot(Re,sp);
 end
 
-legend('N=34','N=67','N=88','N=96','N=108','N=109','location','Best');
+legend('FOM','N=34','N=67','N=88','N=96','N=108','N=109','location','Best');
 
 figure()
+fID = fopen('EspectroFOM.txt','r');
+vec = fscanf(fID, '%f');
+plot(Re,sp,'--','Linewidth',2)
+hold on
 for i=2:length(NN)
     fID = fopen(['DeltaN_',num2str(NN(i)),'.txt'],'r');
     vec = fscanf(fID, '%f');
     sp = vec(2:end); %El primer valor es el tamaño que no interesa, y el segundo vale cero que no lo pinta
     plot(Re,sp);
-    hold on
 end
 
 grid on
-legend('N=67','N=88','N=96','N=108','N=109','location','Best');
+legend('FOM','N=67','N=88','N=96','N=108','N=109','location','Best');
