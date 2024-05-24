e30459e79df0e50f4650e02f9c98991d4d07a177
Alejandro Bandera <abandera@us.es>
2024-05-24 15:39:43 +0200

    Pruebas en el caso con Re = 100

--- _Alejandro/LPSpen/Online/Errores/RepresentacionEnergia.m
+++ _Alejandro/LPSpen/Online/Errores/RepresentacionEnergia.m
@@ -3,8 +3,8 @@
 clear, clc, close all
 
 %% Lectura de datos
-Re = 200;
-tol = 4;
+Re = 100;
+tol = 8;
 % Datos POD
 errorPOD = openfile(['ErrorPOD_Re_',num2str(Re),'_PODtol_',num2str(tol),'.txt']);
 errorPOD = errorPOD(1:end-1);
@@ -12,7 +12,7 @@ errorPOD = errorPOD(1:end-1);
 errorPEN = openfile(['ErrorPEN_Re_',num2str(Re),'_PODtol_',num2str(tol),'.txt']);
 errorPEN = errorPEN(1:end-1);
 % Datos FIL
-for i=1:4
+for i=1:5
     errorFIL(i,:) = openfile(['ErrorFIL_',num2str(i),'_Re_',num2str(Re),'_PODtol_',num2str(tol),'.txt']);
 end
 %errorFIL(4,:) = openfile(['ErrorFIL_',num2str(10),'_Re_',num2str(Re),'_PODtol_',num2str(tol),'.txt']);
@@ -32,9 +32,9 @@ figure()
 hold on
 plot(t,errorPOD(1:nt));
 plot(t,errorPEN(1:nt));
-for i=1:4
+for i=1:5
     plot(t,errorFIL(i,1:nt));
 end
 grid on
 
-legend('POD','PEN','FIL 1','FIL 2','FIL 3','FIL 4');
+legend('POD','PEN','FIL 1','FIL 2','FIL 3','FIL 4','FIL 5');
