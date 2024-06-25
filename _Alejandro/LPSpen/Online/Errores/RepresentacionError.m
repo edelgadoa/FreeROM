b4d390381b74c587da83279c3a547a3673f93afb
Alejandro Bandera <abandera@us.es>
2024-06-25 12:59:06 +0200

    Diferentes pruebas con el cambio de estabilizador

--- _Alejandro/LPSpen/Online/Errores/RepresentacionError.m
+++ _Alejandro/LPSpen/Online/Errores/RepresentacionError.m
@@ -4,7 +4,7 @@ clear, clc, close all
 
 %% Lectura de datos
 Re = 100;
-tol = 8;
+tol = 4;
 % Datos POD
 errorPOD = openfile(['ErrorPOD_Re_',num2str(Re),'_PODtol_',num2str(tol),'.txt']);
 errorPOD = errorPOD(1:end-1);
@@ -15,6 +15,8 @@ errorPEN = errorPEN(1:end-1);
 for i=1:5
     errorFIL(i,:) = openfile(['ErrorFIL_',num2str(i),'_Re_',num2str(Re),'_PODtol_',num2str(tol),'.txt']);
 end
+errorFIL(6,:) = openfile(['ErrorFIL_',num2str(10),'_Re_',num2str(Re),'_PODtol_',num2str(tol),'.txt']);
+%errorFIL(7,:) = openfile(['ErrorFIL_',num2str(20),'_Re_',num2str(Re),'_PODtol_',num2str(tol),'.txt']);
 %errorFIL(4,:) = openfile(['ErrorFIL_',num2str(10),'_Re_',num2str(Re),'_PODtol_',num2str(tol),'.txt']);
 
 errorFIL = errorFIL(:,1:end-1);
@@ -32,9 +34,9 @@ figure()
 hold on
 plot(t,errorPOD(1:nt));
 plot(t,errorPEN(1:nt));
-for i=1:5
+for i=1:6
     plot(t,errorFIL(i,1:nt));
 end
 grid on
 
-legend('POD','PEN','FIL 1','FIL 2','FIL 3','FIL 4','FIL 5');
+legend('POD','PEN','FIL 1','FIL 2','FIL 3','FIL 4','FIL 5','FIL 10','FIL 20');
